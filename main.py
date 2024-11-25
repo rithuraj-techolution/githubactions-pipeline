0019e55fb750487c91a53cc4172e80a862d44ec3
rithuraj-techolution <rithuraj.nambiar@techolution.com>
2024-11-25 04:45:12 +0000

    Refactored code updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+ 
 from colorama import Fore
 
-print(Fore.RED + "Hello World" + Fore.RESET)
-
+# Print "Hello World" in red color and reset the color afterwards
+print(f"{Fore.RED}Hello World{Fore.RESET}")
