bb859d1b8733dee7de38686bcf493d285036888f
Rithuraj Nambiar <155671971+rithuraj-techolution@users.noreply.github.com>
2024-11-25 12:38:25 +0530

    Update utils.py --test
    Update utils.py --test

--- test2/utils.py
+++ test2/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 data = {
     'Name': ['Alice', 'Bob', 'Charlie', 'David'],
     'Age': [24, 27, 22, 32],
+    
     'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
 }
 
