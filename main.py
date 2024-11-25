7503fc47769bd44cf73825747bba049281def074
rithuraj-techolution <rithuraj.nambiar@techolution.com>
2024-11-25 10:08:49 +0000

    Refactored code updated

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from colorama import Fore
 
-print(Fore.RED + "Hello World" + Fore.RESET)
+from colorama import Fore, init
 
+# Initialize colorama for automatic resetting of color
+init(autoreset=True)
+
+def print_colored_message(message, color):
+    """
+    Print the given message in the specified color.
+
+    :param message: The message to print
+    :param color: The color in which the message should be printed
+    """
+    print(color + message)
+
+# Print "Hello World" in red
+print_colored_message("Hello World", Fore.RED)
